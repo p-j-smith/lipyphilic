b062c4178a0bc527edaa6d8baecd0132dbe2eb33
Paul <paul.smith@kcl.ac.uk>
2021-02-19 11:18:56 +0000

    Add count_neighbours method

--- src/lipyphilic/lib/neighbours.py
+++ src/lipyphilic/lib/neighbours.py
@@ -87,8 +87,10 @@ The class and its methods
     :members:
 
 """
+from tqdm.auto import tqdm
 import numpy as np
 import scipy.sparse
+import pandas as pd
 from MDAnalysis.lib.distances import capped_distance
 
 from lipyphilic.lib import base
@@ -118,7 +120,7 @@ class Neighbours(base.AnalysisBase):
         ---
         
         The resultant sparse matrix can be used to calculate the number of each lipid species
-        neighbouring each individual lipid at each frame using :class:`lipyphilic.lib.neighbours.NumNeighbours`
+        neighbouring each individual lipid at each frame using :func:`lipyphilic.lib.neighbours.count_neighbours`
         
         """
         self.u = universe
@@ -155,7 +157,7 @@ class Neighbours(base.AnalysisBase):
         # Currently we have pairs of atoms
         ref, neigh = np.unique(self.membrane.resindices[pairs], axis=0).T
         
-        # Dont keep self-interactions between lipid
+        # Dont keep self-interactions between lipids
         different = ref != neigh
         ref = ref[different]
         neigh = neigh[different]
@@ -170,3 +172,91 @@ class Neighbours(base.AnalysisBase):
         # csc sparse matrices are faster for slicing columns
         # we'll want this for selecting neighbours at given frames
         self.neighbours = self.neighbours.tocsc()
+
+    def count_neighbours(self, fractional_enrichment=False, count_by=None):
+        """Count the number of each neighbour type at each frame.
+
+        Parameters
+        ----------
+        fractional_enrichment : bool
+            If true, will also calculate the fractional enrichment of each lipid at each frame,
+            defined as [A_local] / [A_bulk], where [A_local] is the number of lipids of a given
+            species neighbouring a reference lipid and [A_bulk] is the molar concentration
+            of this lipid species in the bilayer. Defaults to None, in which case the fractional
+            enrichment is not calculated.
+        count_by : numpy.ndarray
+            An array containing ordinal data describing each lipid at each frame. For example,
+            it may be an array containing information on the ordered state or each lipid.
+            Defaults to None, in which case the lipid species
+            (resnames) are used for counting neighbours.
+        count_by_labels : list
+            A dictionary of labels describing what each unique value in `count_by` refers to, e.g
+            if `count_by` contains information on the ordered state of each lipid at each frame, whereby
+            0 corresponds to disordered and 1 corresponds to disordered, then
+            count_by_labels = {0: 'Ld', 1: 'Lo'}. There must be precisely one label for each unique
+            value in 'count_by'.
+        
+        Returns
+        -------
+        
+        counts : pandas.DataFrame
+            A DataFrame containing the following data for each lipid at each frame: lipid identifier,
+            frame number, number of neighbour of each species (or of each type in :attr:'count_by' if)
+            this is provided, as well as the total number of neighbours. If :attr:'fractional_enrichment'
+            is true
+            
+        Note
+        ----
+        
+        Neighbours must be found by using `Neighbours.run()` before calling this method.
+        """
+        
+        # create output array
+        all_resnames = self.membrane.resnames
+        unique_resnames = np.unique(all_resnames)
+        all_counts = np.full(
+            (self.membrane.n_residues, self.n_frames, unique_resnames.size),
+            fill_value=0,
+            dtype=np.uint8  # count can't be negative, and no lipid will have more than 255 neighbours
+        )
+        
+        if fractional_enrichment:
+            all_fe = np.full(
+                (self.membrane.n_residues, self.n_frames, unique_resnames.size),
+                fill_value=0,
+                dtype=float
+            )
+        
+        # For counts we need to know which column of the output array to add counts to for each lipid type
+        type_index = {resname: i for i, resname in enumerate(unique_resnames)}
+        
+        # Get counts at each frame
+        n_residues = self.membrane.n_residues
+        for frame_index in tqdm(np.arange(self.n_frames)):
+        
+            ref, neigh = self.neighbours[:, frame_index * n_residues:(frame_index + 1) * n_residues].nonzero()
+            
+            unique, counts = np.unique([ref, [type_index[t] for t in all_resnames[neigh]]], axis=1, return_counts=True)
+            r, t = unique  # reference index (r) and type index (t)
+            all_counts[r, frame_index, t] = counts
+
+        # Assemble data for the DataFrame
+        all_counts = all_counts.reshape(n_residues * self.n_frames, unique_resnames.size)
+        total_counts = np.sum(all_counts, axis=1)
+
+        frames = np.full((n_residues, self.n_frames), fill_value=self.frames)
+        frames = frames.reshape(n_residues * self.n_frames)
+
+        resnames = np.full((n_residues, self.n_frames), fill_value=self.membrane.residues.resnames[:, np.newaxis])
+        resnames = resnames.reshape(n_residues * self.n_frames)
+        
+        data = np.concatenate((resnames[:, np.newaxis], frames[:, np.newaxis], all_counts, total_counts[:, np.newaxis]), axis=1)
+        
+        # Create DataFrame
+        columns = ["Resname", "Frame"] + ["n" + lipid for lipid in unique_resnames] + ["Total"]
+        df = pd.DataFrame(
+            data=data,
+            columns=columns
+        )
+        
+        return df
