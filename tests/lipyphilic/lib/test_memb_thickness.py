9a58d0a28e791805f3a225369cb830340089eb0d
Paul <paul.smith@kcl.ac.uk>
2021-03-15 10:48:43 +0000

    Add tests for memb_thickness.py

---
+++ tests/lipyphilic/lib/test_memb_thickness.py
@@ -0,0 +1,74 @@
+
+import pytest
+import numpy as np
+import MDAnalysis
+
+from numpy.testing import assert_array_equal
+
+from lipyphilic._simple_systems.simple_systems import (
+    HEX_LAT, HEX_LAT_BUMP)
+from lipyphilic.lib.memb_thickness import MembThickness
+ 
+ 
+class TestMembThickness:
+    
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def universe():
+        return MDAnalysis.Universe(HEX_LAT)
+
+    leaflets = np.ones((100, 1))
+    leaflets[50:] = -1
+
+    kwargs = {
+        'lipid_sel': 'name L C',
+        'leaflets': leaflets
+    }
+
+    def test_memb_thickness(self, universe):
+    
+        memb_thickness = MembThickness(universe, **self.kwargs)
+        memb_thickness.run()
+    
+        reference = {
+            'n_frames': 1,
+            'thickness': [20]
+        }
+    
+        assert memb_thickness.memb_thickness.shape == (reference['n_frames'], )
+        assert_array_equal(memb_thickness.memb_thickness, reference['thickness'])
+
+
+class TestMembThicknessUndulating:
+    
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def universe():
+        return MDAnalysis.Universe(HEX_LAT_BUMP)
+
+    leaflets = np.ones((100, 1))
+    leaflets[50:] = -1
+
+    kwargs = {
+        'lipid_sel': 'name L C',
+        'leaflets': leaflets
+    }
+    
+    reference = {
+        'thickness': [20],
+        'thickness_grid': np.full((1, 4, 4), fill_value=20)  # 1 frame with 4 by 4 membrane grid
+    }
+       
+    def test_nbins4(self, universe):
+        
+        memb_thickness = MembThickness(universe, n_bins=4, **self.kwargs)
+        memb_thickness.run()
+    
+        assert_array_equal(memb_thickness.memb_thickness, self.reference['thickness'])
+        
+    def test_return_surface(self, universe):
+        
+        memb_thickness = MembThickness(universe, n_bins=4, return_surface=True, **self.kwargs)
+        memb_thickness.run()
+    
+        assert_array_equal(memb_thickness.memb_thickness_grid, self.reference['thickness_grid'])
