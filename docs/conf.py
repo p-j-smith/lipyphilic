5aad5493ede914fef2a9e1f69464cdd62789a81f
Paul <paul.smith@kcl.ac.uk>
2021-03-26 11:52:58 +0000

    Bump version: 0.5.0 → 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project = 'lipyphilic'
 year = '2021'
 author = 'Paul Smith'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.5.0'
+version = release = '0.6.0'
 
 autodoc_typehints = 'signature'
 autodoc_docstring_signature = True
