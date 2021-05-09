4a861b3c7c0be981e08d131204ada0f1d2f15084
Paul <paul.smith@kcl.ac.uk>
2021-05-09 19:42:46 +0100

    Bump version: 0.6.2 → 0.6.3

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project = 'lipyphilic'
 year = '2021'
 author = 'Paul Smith'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.6.2'
+version = release = '0.6.3'
 
 autodoc_typehints = 'signature'
 autodoc_docstring_signature = True
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='lipyphilic',
-    version='0.6.2',
+    version='0.6.3',
     license='LGPL-2.1-or-later',
     description='Analyse MD simulations of lipids with python',
     long_description='%s\n%s' % (
