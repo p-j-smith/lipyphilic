589842db4cf139e698e5d5fcea52d7d1fa6e78ae
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-03-19 15:30:49 +0000

    Add deprecation warning for plotting classes

---
+++ src/lipyphilic/lib/plotting.py
@@ -0,0 +1,49 @@
+import warnings
+
+_msg = (
+    "The module `lipyphilic.lib.plotting` is deprecated and will be removed "
+    "in a later version. Please use `lipyphilic.plotting` instead."
+)
+warnings.warn(
+    _msg,
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+
+class ProjectionPlot:
+    """The class is deprecated. Please use `lipyphilic.plotting.ProjectionPlot instead`."""
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        _msg = (
+            "`lipyphilic.lib.plotting.ProjectionPlot` is deprecated and will be removed "
+            "in a later version. Please instead use `lipyphilic.plotting.ProjectionPlot` instead."
+        )
+        warnings.warn(
+            _msg,
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
+
+class JointDensity:
+    """This class is deprecated. Please use `lipyphilic.plotting.JointDensity` instead.`"""
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        _msg = (
+            "`lipyphilic.lib.plotting.JointDensity` is deprecated and will be removed "
+            "in a later version. Please instead use `lipyphilic.plotting.JointDensity` instead."
+        )
+        warnings.warn(
+            _msg,
+            DeprecationWarning,
+            stacklevel=2,
+        )
