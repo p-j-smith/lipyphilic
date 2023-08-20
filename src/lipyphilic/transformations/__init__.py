b845eefce9ed76545b470df7e64b62e25e1a5080
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-08-20 11:30:21 +0100

    Ensure nojump can still be imported from lipyphilic.transformations until it is removed

--- src/lipyphilic/transformations/__init__.py
+++ src/lipyphilic/transformations/__init__.py
@@ -1,6 +1,7 @@
-from lipyphilic.transformations.transformations import center_membrane, triclinic_to_orthorhombic
+from lipyphilic.transformations.transformations import center_membrane, nojump, triclinic_to_orthorhombic
 
 __all__ = [
     "center_membrane",
     "triclinic_to_orthorhombic",
+    "nojump",
 ]
