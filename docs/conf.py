bb01fdf2a758bbb8ecfe078dd10e6354bf45995c
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-11-30 16:21:57 +0000

    build all formats for rtd, and always set rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import sys
 
@@ -42,12 +41,8 @@ extlinks = {
     "pr": ("https://github.com/p-j-smith/lipyphilic/pull/%s", "PR #"),
     "mda": ("https://www.mdanalysis.org", "MDAnalysis"),
 }
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only set the theme if we're building docs locally
-    html_theme = "sphinx_rtd_theme"
-
+html_theme = "sphinx_rtd_theme"
 html_logo = "logo/lipyphilic_logo_grey.png"
 html_use_smartypants = True
 html_last_updated_fmt = "%b %d, %Y"
